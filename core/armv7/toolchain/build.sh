665ac094d479d5b52e05810a967b95169271fad6
Andrew Danger Lyon <orthecreedence@gmail.com>
2018-08-20 11:36:18 -0700

    rebuild android ndk boxes to target SDK v23 (from v26).

--- core/armv7/toolchain/build.sh
+++ core/armv7/toolchain/build.sh
@@ -7,9 +7,9 @@ cd /tmp/android-sdk
 wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
 unzip sdk-tools-linux-4333796.zip
 cd tools
-echo 'y' | ./bin/sdkmanager 'platforms;android-26' 'ndk-bundle'
+echo 'y' | ./bin/sdkmanager 'platforms;android-23' 'ndk-bundle'
 cd ../ndk-bundle
-./build/tools/make_standalone_toolchain.py --arch arm --api 26 --install-dir /opt/gcc-armv7-android
+./build/tools/make_standalone_toolchain.py --arch arm --api 23 --install-dir /opt/gcc-armv7-android
 cd
 rm -rf /tmp/android-sdk
 

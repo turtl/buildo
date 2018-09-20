5d0f0a15127cc380a97515056c745a734a618210
Andrew Danger Lyon <orthecreedence@gmail.com>
2018-09-20 15:18:47 -0700

    core arm builds: download ndk v17 directly instead of using stinkin sdk manager (v18+ has no gcc and i dont care to patch openssl to support this shit)

--- core/armv7/toolchain/build.sh
+++ core/armv7/toolchain/build.sh
@@ -7,8 +7,13 @@ cd /tmp/android-sdk
 wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
 unzip sdk-tools-linux-4333796.zip
 cd tools
-echo 'y' | ./bin/sdkmanager 'platforms;android-23' 'ndk-bundle'
-cd ../ndk-bundle
+echo 'y' | ./bin/sdkmanager 'platforms;android-23'
+# note that we can't use the latest NDK because it doesn't include GCC,
+# and we need a GCC toolchain to compile openssl
+wget https://dl.google.com/android/repository/android-ndk-r17c-linux-x86_64.zip
+unzip android-ndk-r17c-linux-x86_64.zip
+mv android-ndk-r17c ndk-bundle
+cd ndk-bundle
 ./build/tools/make_standalone_toolchain.py --arch arm --api 23 --install-dir /opt/gcc-armv7-android
 cd
 rm -rf /tmp/android-sdk
